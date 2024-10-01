dfeca17635e68ba3bff3f234ec9776450217f9d2
jormaraty <jorma.raty@metropolia.fi>
2024-10-01 20:32:50 +0300

    Toinen versio

--- kertaus/T3.py
+++ kertaus/T3.py
@@ -1,4 +1,30 @@
-"""
-Tänne tulee tietokantatehtäviä
-"""
+'''
+Vastaukset tehtäviin löytyvät tiedostosta V3
+'''
+
+# 1. Käy läpi python-teorian kohta 8 'Relaatiotietokannan käyttö'
+# ja erityisesti sen kohta 'Hakukysely ja tulosjoukon käsittely'
+# https://github.com/vesavvo/Python_Ohjelmistoteema/blob/main/08_Relaatiotietokannan_k%C3%A4ytt%C3%B6.md#hakukysely-ja-tulosjoukon-k%C3%A4sittely
+
+
+
+# 2. Tee seuraavista lauseista kelvollinen ohjelma,
+#    joka hakee tietokannan taulusta dataa ja tulostaa saamansa tiedot.
+#    Poimi seuraavista lauseista 6 olennaista kohtaa oikeaan järjestykseen.
+
+'''
+print(formatted_db_data)
+import mysql.connector
+connection = mysql.connector(...)
+import mysql.helper
+result = cursor.fetchall()
+cursor.execute("SELECT etunimi, sukunimi FROM tyontekija WHERE ...")
+print(result[0] [1])
+result = cursor.getResult()
+
+'''
+
+
+
+
 

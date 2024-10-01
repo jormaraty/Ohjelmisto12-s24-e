47784782e97920813943b2341f5073acb4465a96
jormaraty <jorma.raty@metropolia.fi>
2024-10-01 20:11:51 +0300

    Toinen versio

--- kertaus/T1.py
+++ kertaus/T1.py
@@ -1,29 +1,23 @@
-# Onko if-ehto totta jos x = 11?
-x = 11
-if x > 5 or x <= 10:
-    print("if-ehto on tosi")
+'''
+Vastaukset tehtäviin löytyvät tiedostosta V1
+'''
 
+# 1. Onko if-ehto 'if x > 5 or x <= 10' tosi/True jos x = 11?
 
-# Muista myös esim:
-    # while-toistoa ei välttämättä suoriteta kertaakaan
-    # while-toisto voi jäädä ikuiseen silmukaan
-
-
-# Mitä seuraava tulostaa?
+# 2. Mitä seuraava tulostaa?
 luku = 1
 while luku % 5 > 0:
-    # tänne tullaan, kun ???
     print(luku)
     luku += 3
 print(luku)
 
-# Onko kelvollinen listarakenne?
+# 3. Mitkä alla olevista muodostavat lista-rakenteen?
 lista1 = (1, 3, 5, -1)
 lista2 = [1, 2, -3, 4]
 lista3 = [1, 'toka', 3, 4]
 
 
-# mitä seuraava tulostaa?
+# 4. Mitä seuraava tulostaa?
 nimet = ['Anna', 'Matti']
 nimet.append('Tiina')
 nimet.append('Mikko')
@@ -33,10 +27,10 @@ print(nimet)
 print(nimet[1:3])
 print(nimet[-1])
 
-# Mitä seuraava tulostaa?
+
+# 5. Mitä seuraava tulostaa?
 people = ("Matti", "Pekka", "Anne")
 anne, maija, pekka = people
 print(anne)
 print(people[1])
 print(people)
-
